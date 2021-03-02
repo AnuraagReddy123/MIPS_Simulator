57cad3fecdb7cc83f23bdf6aa5b40bb3b7b4c998
P. Anuraag Reddy <anuraagreddy74@outlook.com>
2021-03-02 21:51:00 +0530

    Global variables added

--- main.py
+++ main.py
@@ -1,28 +1,13 @@
-from operations import add,jump,sub,bne,load,store
+import globals
+from operations import *
 
-instructions = [] # list of the instructions read from the file
-label_dict = {'loop' : '2'} # To store the indices of where the labels are occurring
-data_dict = {'value' : 2} # storing the indices for data segment
-comments = {}
-
-base_address = int("0x10010000",16) # address of the first byte
-
-registers = {'$r0' : '00000000','$at' : '00000000','$v0' : '00000000','$v1' : '00000000',
-                  '$a0' : '00000000','$a1' : '00000000','$a2' : '00000000','$a3' : '00000000',
-                  '$t0' : '10010000','$t1' : '00011223','$t2' : '00000000','$t3' : '00000000',
-                  '$t4' : '00000000','$t5' : '00000000','$t6' : '00000000','$t7' : '00000000',
-                  '$s0' : '00000000','$s1' : '00000000','$s2' : '00000000','$s3' : '00000000',
-                  '$s4' : '00000000','$s5' : '00000000','$s6' : '00000000','$s7' : '00000000',
-                  '$t8' : '00000000','$t9' : '00000000','$k0' : '00000000','$k1' : '00000000',
-                  '$gp' : '00000000','$sp' : '00000000','$s8' : '00000000','$ra' : '00000000',}
-
-data_segment = ['00000000'] * 1024 #data segment (4 Kilobytes)
+globals.initialize()
 
 def find_label (instr, instr_num):
     label_index = instr.find(":")
     if label_index != -1:
         label = instr[:label_index]
-        label_dict[label] = instr_num
+        globals.label_dict[label] = instr_num
         instr = instr.replace(instr[:label_index+1], "")
     return instr
 
@@ -33,7 +18,7 @@ def clean_instruction(instr, instr_num):
     #Searching for comments
     comment_index = instr.find('#')
     if comment_index != -1:
-        comments[instr[comment_index:]] = instr_num
+        globals.comments[instr[comment_index:]] = instr_num
         instr = instr.replace(instr[comment_index:], "")
         instr = instr.strip()
     
@@ -48,12 +33,16 @@ if __name__ == "__main__":
             instr = clean_instruction(instr, instr_num)
             
             if (instr != ""):
-                instructions.append(instr)
+                globals.instructions.append(instr)
                 instr_num += 1
         
-        print(comments)
+        print(globals.comments)
         print("\n")
-        print(instructions)
+        print(globals.instructions)
         print("\n")
-        print(label_dict)
-        written = "sw $t1, value" # test the instruction
+        print(globals.label_dict)
+    
+    pc = 0
+    written = "sub $t0, $t0, $t1" # test the instruction
+    pc = sub(pc, written)
+    print(globals.registers)
--- operations.py
+++ operations.py
@@ -1,4 +1,4 @@
-from main import instructions,data_segment,data_dict,label_dict,base_address
+import globals
 
 def add(regs, s):
     reg1 = int(s.split()[2][2])
@@ -15,20 +15,20 @@ def load(register, data, s):
     mem = s.split()[2]
     register[r] = data[mem]
 
-def sub(PC,code,registers):
+def sub(PC,code):
     code = code.replace(" ","") #get rid of whitespaces
     index = code.find('$') #find first occurrence of $
     reg_code = code[index:] # get the list of registers used
     fetched_registers = reg_code.split(",") # split the registers to access individually 
     # perform the subtraction operation on the registers and get the answer in decimal
-    temp_ans = int(registers[fetched_registers[1]],16) - int(registers[fetched_registers[2]],16)
+    temp_ans = int(globals.registers[fetched_registers[1]],16) - int(globals.registers[fetched_registers[2]],16)
     hex_ans = hex(temp_ans) #convert the integer back into hex form
      # store the hex back into registers and get rid of 0x in the beginning
-    registers[fetched_registers[0]] = hex_ans[2:]
-    registers[fetched_registers[0]] = registers[fetched_registers[0]].rjust(8,'0')
+    globals.registers[fetched_registers[0]] = hex_ans[2:]
+    globals.registers[fetched_registers[0]] = globals.registers[fetched_registers[0]].rjust(8,'0')
     return PC+1
 
-def bne(PC,code,registers,label_dict):# for branch not equal instruction
+def bne(PC,code):# for branch not equal instruction
     code = code.replace(" ","") # get rid of whitespaces
     index = code.find('$') # find the first occurrence of $
     reg_code = code[index:] # get the list of registers used
@@ -36,51 +36,51 @@ def bne(PC,code,registers,label_dict):# for branch not equal instruction
     first_register = fetched_registers[0] # get the first register
     second_register = fetched_registers[1] # get the second register
     jump_target = fetched_registers[2] # the jump target
-    if registers[first_register] == registers[second_register]: # if the contents are equal
+    if globals.registers[first_register] == globals.registers[second_register]: # if the contents are equal
        PC = PC+1
     else:
-       PC = label_dict[jump_target] # assign the PC
+       PC = globals.label_dict[jump_target] # assign the PC
     return PC
 
-def store(PC,code,data_segment,registers,data_dict): # for store word instruction
+def store(PC,code): # for store word instruction
     code = code.replace(" ","") # get rid of whitespaces
     index = code.find('$') # find the first occurrence of $
     reg_code = code[index:] # get the list of registers used
     fetched_registers = reg_code.split(",") # split the registers to access individually
-    word = registers[fetched_registers[0]] # get the word to be stored
+    word = globals.registers[fetched_registers[0]] # get the word to be stored
     if fetched_registers[1].find('(') == -1: # if variable name is given instead of address
         target_name = fetched_registers[1]
-        data_segment[data_dict[target_name]] = word 
+        globals.data_segment[globals.data_dict[target_name]] = word 
     else:
         jump = int(fetched_registers[1][0:fetched_registers[1].find('(')])  # number of bytes to skip
         address_register = fetched_registers[1][fetched_registers[1].find('(')+1:fetched_registers[1].find(')')] # fetch the address register
-        dest_index = int(registers[address_register],16) + jump - base_address
+        dest_index = int(globals.registers[address_register],16) + jump - globals.base_address
         dest_index = dest_index // 4 # get the destination index
-        data_segment[dest_index] = word
+        globals.data_segment[dest_index] = word
     return PC+1 # PC for next instruction
 
-if __name__ == "__main__":
-    registers = [1, 2, 3, 0, 0, 0]                  # 6 Registers (r0, r1, r2, ... r6) Assume r3 register not for use Map
+# if __name__ == "__main__":
+#     registers = [1, 2, 3, 0, 0, 0]                  # 6 Registers (r0, r1, r2, ... r6) Assume r3 register not for use Map
        
-    data = {"g": 5, "h": 4, "i": 3}                 #Store data in list
+#     data = {"g": 5, "h": 4, "i": 3}                 #Store data in list
 
-    instructions = {"add $r0, $r0, $r1", "ld $r0, g"}
-    label_dict = {"main":0, "load": 1}
+#     instructions = {"add $r0, $r0, $r1", "ld $r0, g"}
+#     label_dict = {"main":0, "load": 1}
 
-    s = "add $r0, $r0, $r1"
-    if s.split()[0] == "add":
-        add(registers, s)
-    print(registers)
+#     s = "add $r0, $r0, $r1"
+#     if s.split()[0] == "add":
+#         add(registers, s)
+#     print(registers)
 
-    s = "jump load"
+#     s = "jump load"
 
-    s = "ld $r0, g"
-    print(s.split())
-    load(registers, data, s)
-    print(registers)
+#     s = "ld $r0, g"
+#     print(s.split())
+#     load(registers, data, s)
+#     print(registers)
 
-    s = "ld             $r0, g"
-    print(s.split())
+#     s = "ld             $r0, g"
+#     print(s.split())
 
-    while():
-        pc = jump()
+#     while():
+#         pc = jump()
