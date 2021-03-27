5380dedeba460cd1b6ae53170defd234f8d0da01
nmnsharma007 <nmnsharma20@gmail.com>
2021-03-27 22:31:29 +0530

    IF added

--- Project_Phase_2/stages.py
+++ Project_Phase_2/stages.py
@@ -1,12 +1,13 @@
-from sim_glob import *
+import sim_glob
 
-def IF(PC,Prev_PC,clock):# instruction fetch in python
-    globals.fetched_instr = instructions[PC] #store the fetched instruction 
-    clock = clock + 1
-    Prev_PC = PC
-    PC = PC + 1
-    next_instruction = {'ID/RF' : [PC,Prev_PC,clock]}
-    queue.append(next_instruction)
+def IF(PC,clock):# instruction fetch in python
+    if not sim_glob.fetched_instr: # if no instruction fetch was going on
+        sim_glob.fetched_instr = sim_glob.instructions[PC] #store the fetched instruction 
+        clock = clock + 1
+        next_instruction = {'IDRF' : clock} # Enqueue the next stage
+    else:
+        next_instruction = {'IF' : [PC,clock+1]}# Enqueue the same instruction
+    sim_glob.queue.append(next_instruction)
 
 def IDRF():
     pass
@@ -14,10 +15,14 @@ def IDRF():
 def EX():
     pass
 
-def MEM():
+def MEM(instruction_type,src_registers,dest_registers,dependent_register,clock):
+    if instruction_type == 2:
+        if not dependent_register:
+                        
     pass
 
 def WB():
     pass
+    
 
     
