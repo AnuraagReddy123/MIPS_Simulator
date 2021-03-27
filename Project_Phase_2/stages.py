544ef16abcd2213ceeccf3e87f226439042d3101
nmnsharma007 <nmnsharma20@gmail.com>
2021-03-28 00:25:02 +0530

    added MEM

--- Project_Phase_2/stages.py
+++ Project_Phase_2/stages.py
@@ -16,9 +16,9 @@ def EX():
     pass
 
 def MEM(instruction_type,src_registers,dest_registers,clock):
+    dest_register = next(iter(dest_registers)) # get the destination register
     if instruction_type == 2:# load instruction
         src_register = next(iter(src_registers)) # get the source register
-        dest_register = next(iter(dest_registers)) # get the destination register
         memory_address = src_registers[src_register]# fetch the memory address in the memory segment
         dest_index = int(memory_address,16)  - sim_glob.base_address
         dest_index = dest_index // 4 # get the destination index
@@ -27,7 +27,6 @@ def MEM(instruction_type,src_registers,dest_registers,clock):
         next_instruction = {'WB': [dest_register,word,clock+1]}
     elif instruction_type == 3:# store instruction
         src_register = next(iter(src_registers)) # get the source register
-        dest_register = next(iter(dest_registers)) # get the destination register
         memory_address = src_registers[src_register]# fetch the memory address in the memory segment
         dest_index = int(memory_address,16)  - sim_glob.base_address
         dest_index = dest_index // 4 # get the destination index
