93454de4f8206849dcd6734f91e8a5bb20588dbd
Anuraag Reddy <anuraagreddy74@outlook.com>
2021-03-29 22:11:30 +0530

    IF Fixed

--- Project_Phase_2/utility_func.py
+++ Project_Phase_2/utility_func.py
@@ -72,6 +72,11 @@ def fetch_reg(instr):
         instr = instr[index+3:]
     return fetched_registers
 
+def fetch_val(instr):
+    instr = instr.replace(" ","")
+    index = instr.find("$")
+    reg = instr[index:].split(",")
+    return reg[1]
 
 def fetch_imm(instr):
     instr = instr.replace(" ", "")
@@ -105,7 +110,7 @@ def clean_instruction(instr, instr_num):
 
 
 if __name__ == "__main__":
-    print(fetch_label("BEQ $R1, $R2, hey"))
+    print(fetch_val("LI $R1, 0x1231"))
     h = []
     class hey:
         def __init__(self, pc,val):
