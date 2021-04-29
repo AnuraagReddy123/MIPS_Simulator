230969b5e58deea2f8e5e5f6bc3eff08e815370b
nmnsharma007 <nmnsharma20@gmail.com>
2021-04-29 23:43:24 +0530

    L2 cache responding finally

--- Project_Phase_3/test5.s
+++ Project_Phase_3/test5.s
@@ -1,6 +1,15 @@
     .text
 main:
-    
+    LI $s0, 0x10010000
+    LOAD $s1, 0($s0)
+    LOAD $s1, 8($s0)
+    LOAD $s1, 12($s0)
+    LOAD $s1, 0($s0)
+    LOAD $s1, 8($s0)
+    LOAD $s1, 12($s0)
+    LOAD $s1, 0($s0)
+    LOAD $s1, 8($s0)
+    LOAD $s1, 12($s0)
 
 #   IF(1)  ID(2)  EX(3)  MEM(4)   WB(5)
 #          IF(2)  ID(3)  EX(4)    MEM(5)                        MEM(104)   WB(105)
